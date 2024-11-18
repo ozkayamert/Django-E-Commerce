54fb6150b95059d5f5dc636bbc3ecb9d42d76f3f
Mert Ozkaya <ozkayamert35@gmail.com>
2024-11-18 13:39:16 +0300

    Creating a model and adding it to the admin panel

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=155)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        verbose_name = "Category"
+
+    def __str__(self):
+        return self.name
