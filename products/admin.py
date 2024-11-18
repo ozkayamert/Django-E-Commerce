5414caf432339e9caf6f103a3623687a4ff19571
Mert Ozkaya <ozkayamert35@gmail.com>
2024-11-18 14:03:01 +0300

    ForeignKey, TextField, SlugField

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Categories
 
-admin.site.register(Categories)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'seo_title', 'slug', 'is_active']
+    list_filter = ['is_active', 'name']
+    search_fields = ['name', 'seo_title', 'slug']
+
+admin.site.register(Categories, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 class Categories(models.Model):
     name = models.CharField(max_length=155)
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, blank= True, null=True)
     is_active = models.BooleanField(default=True)
+    seo_title = models.CharField(max_length=155, blank=True, null=True)
+    seo_description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(max_length=155, unique=True, null=True, blank=True)
 
     class Meta:
         verbose_name_plural = "Categories"
