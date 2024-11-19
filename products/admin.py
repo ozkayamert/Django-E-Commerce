3feb0bb030ceaadf4bcb0baa2c539c54f97a2ebb
Mert Ozkaya <ozkayamert35@gmail.com>
2024-11-19 14:46:37 +0300

    Add product, Many to Many

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Categories, Brands
+from .models import Categories, Brands, Products, Tags
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'seo_title', 'slug', 'is_active']
@@ -8,9 +8,20 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Categories, CategoryAdmin)
 
+
 class BrandsAdmin(admin.ModelAdmin):
     list_display = ['name','seo_title','slug','is_active','picture']
     list_filter = ['is_active','name']
     search_fields = ['name','seo_title','slug']
 
 admin.site.register(Brands,BrandsAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','price','brand','category','discount_price','is_active','image','date']
+    list_filter = ['is_active','name','category','brand']
+    search_fields = ['name','seo_title','slug']
+
+admin.site.register(Products, ProductAdmin)
+
+admin.site.register(Tags)
--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Categories(models.Model):
     name = models.CharField(max_length=155)
@@ -27,6 +28,43 @@ class Brands(models.Model):
     class Meta:
         verbose_name_plural = "Brands"
         verbose_name = "Brand"
+
+    def __str__(self):
+        return self.name
+    
+class Tags(models.Model):
+    name = models.CharField(max_length=155)
+    slug = models.SlugField(max_length=155,unique=True,null=True,blank=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = 'Tags'
+        verbose_name = 'Tag'
+
+    def __str__(self):
+        return self.name
+
+    
+class Products(models.Model):
+    name = models.CharField(max_length=155)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    brand = models.ForeignKey(Brands,on_delete=models.CASCADE)
+    category = models.ForeignKey(Categories,on_delete=models.CASCADE)
+    discount_price = models.DecimalField(max_digits=10,decimal_places=2,blank=True,null=True)
+    short_description = models.TextField(blank=True,null=True)
+    description = models.TextField(blank=True,null=True)
+    seo_title = models.CharField(max_length=155,blank=True,null=True)
+    seo_description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(max_length=155, unique=True, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to="productimages",blank=True,null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tags,blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        verbose_name = 'Product'
 
     def __str__(self):
         return self.name
