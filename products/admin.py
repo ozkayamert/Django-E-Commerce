7da102f4df4589e562c5a4db3c7d985d716abca9
Mert Ozkaya <ozkayamert35@gmail.com>
2024-11-19 14:11:00 +0300

    ImageField,MEDIA_URL,MEDIA_ROOT

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Categories
+from .models import Categories, Brands
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'seo_title', 'slug', 'is_active']
@@ -7,3 +7,10 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['name', 'seo_title', 'slug']
 
 admin.site.register(Categories, CategoryAdmin)
+
+class BrandsAdmin(admin.ModelAdmin):
+    list_display = ['name','seo_title','slug','is_active','picture']
+    list_filter = ['is_active','name']
+    search_fields = ['name','seo_title','slug']
+
+admin.site.register(Brands,BrandsAdmin)
--- products/models.py
+++ products/models.py
@@ -14,3 +14,19 @@ class Categories(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Brands(models.Model):
+    name = models.CharField(max_length=155)
+    description = models.TextField(blank=True,null=True)
+    seo_title = models.CharField(max_length=155,blank=True,null=True)
+    seo_description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(max_length=155, unique=True, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to="brand-pictures", blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Brands"
+        verbose_name = "Brand"
+
+    def __str__(self):
+        return self.name
